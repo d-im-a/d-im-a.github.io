f3e3c49875b8ab601d42d4245bd6691c4088fe84
Dima <dima@dima.local>
2024-11-26 00:23:14 +0100

    temp again

--- main.js
+++ main.js
@@ -1,23 +1,57 @@
-const tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: 'section',
-    scrub: 1.5,
-    start: '50% bottom',
-    end: '90% bottom',
-  }
-});
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'; 
+const canvas = document.querySelector( '#c' );
 
-const wiggleTl = gsap.timeline({paused: true, yoyo: true, repeat: -1})
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 50, 2, 0.1, 1000 );
+camera.position.set(0, 50, 0)
 
-tl.from(".stem", .8, {drawSVG:0}, 0);
-tl.from(".petal1", 1, {drawSVG:1}, '-=.0');
-tl.from(".petal2", 1, {drawSVG:1}, '-=.0');
-tl.from(".petal3", 1, {drawSVG:0}, '-=.6');
-tl.from(".petal4", 1, {drawSVG:0}, '-=.6');
-tl.add(function() {
-  // wiggleTl.play();
-}), '<';
-tl.from(".petal5", 1, {drawSVG:0}, '-=.6');
+const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
 
-wiggleTl.to('svg', {rotate: 20, duration: 4, ease: Power2.easeInOut});
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshPhongMaterial( { color: 0x00ff00, emmisive: 'purple', shininess: 0.5 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
+var light = new THREE.DirectionalLight( 0xffffff );
+light.position.set( 0, 1, 1 ).normalize();
+scene.add(light);
+
+var light2 = new THREE.DirectionalLight( 0xff0000 );
+light2.position.set( 0, 1, 1 ).normalize();
+scene.add(light2);
+
+const loader = new GLTFLoader();
+
+loader.load('./assets/ballerina.glb', function ( object ) {
+
+console.log(object)
+/	scene.add( object.scene );
+
+	object.scene.traverse((mesh) => {
+	  // You can also check for id / name / type here.
+	  mesh.material = new THREE.MeshStandardMaterial({ color: 'blue' });
+console.log(mesh)
+	});
+object.scene.rotation.set(0, 90, 0)
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
+camera.position.z = 5;
+camera.position.y = 0;
+camera.position.x = 0;
+
+function animate() {
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+
+}
