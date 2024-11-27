4e2d55cb3653cf485b9f9ccb608a6e184ce72601
Dima <dima@dima.local>
2024-11-27 23:32:17 +0100

    add background, update rotation

--- main.js
+++ main.js
@@ -3,26 +3,24 @@ import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 const canvas = document.querySelector("#c");
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color().setHex(0x303030);
+// scene.background = new THREE.Color().setHex(0x303030);
 
 const camera = new THREE.PerspectiveCamera(50, 2, 0.1, 1000);
-camera.position.set(0, 6, 14);
+camera.position.set(0, 6, 13);
 camera.lookAt(0, 5, 0);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+renderer.setClearColor(0x000000, 0); // the default
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
 
 var floorMaterial = new THREE.MeshBasicMaterial();
 var textureLoader = new THREE.TextureLoader();
 textureLoader.load("./assets/texture.jpg", function (texture) {
-  const floorLength = 200;
-  // The texture has loaded, so assign it to your material object. In the
-  // next render cycle, this material update will be shown on the plane
-  // geometry
+  const floorLength = 300;
   texture.wrapS = THREE.RepeatWrapping;
   texture.wrapT = THREE.RepeatWrapping;
-  texture.repeat.set(floorLength / 8, floorLength / 8)
+  texture.repeat.set(floorLength / 8, floorLength / 8);
   floorMaterial.map = texture;
   floorMaterial.needsUpdate = true;
 
@@ -32,6 +30,7 @@ textureLoader.load("./assets/texture.jpg", function (texture) {
 
   scene.add(floor);
 });
+
 // const planeGeometry = new THREE.PlaneGeometry(200, 200);
 // const planeMaterial = new THREE.MeshPhongMaterial({
 //   color: 0x0000ff,
@@ -39,19 +38,20 @@ textureLoader.load("./assets/texture.jpg", function (texture) {
 // });
 // const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 
-var light = new THREE.DirectionalLight(0x00f000);
-light.position.set(-10, 10, 10);
-light.intensity = 1;
-scene.add(light);
+var inFrontLeftLight = new THREE.DirectionalLight(0x886eff);
+inFrontLeftLight.position.set(-20, 10, 10);
+inFrontLeftLight.intensity = 2;
+scene.add(inFrontLeftLight);
 
-var light2 = new THREE.DirectionalLight(0x0000f0);
-light2.position.set(10, 10, 10);
-light2.intensity = 10;
-scene.add(light2);
+var inFrontRightLight = new THREE.DirectionalLight(0xe0b4fa);
+inFrontRightLight.position.set(14, 10, 10);
+inFrontRightLight.intensity = 4;
+scene.add(inFrontRightLight);
 
-// var light2 = new THREE.DirectionalLight( 0xff0000 );
-// light2.position.set( 0, 1, 1 ).normalize();
-// scene.add(light2);
+const behindLight = new THREE.PointLight(0xfff, 10, 0);
+behindLight.position.set(0, 10, -5);
+behindLight.intensity = 1000;
+scene.add(behindLight);
 
 const gltfLoader = new GLTFLoader();
 
@@ -93,60 +93,55 @@ gltfLoader.load(
   },
 );
 
-let initRotation = 0;
+let startGrabRotation = 0;
+let accumRotation = 0;
 function animate() {
-
   // if (ballerina) {
-    // ballerina.rotation.z += 0.015;
+  // ballerina.rotation.z += 0.015;
   // }
   if (!ballerina) {
-	  return;
+    return;
   }
 
   if (startOfGrabX) {
-	  // console.log(currentGrabX , startOfGrabX)
-	  const grabDiff = currentGrabX - startOfGrabX
-	  const rotateAngle = grabDiff * 2 * Math.PI / canvas.offsetWidth
+    const grabDiff = currentGrabX - startOfGrabX;
+    const rotateAngle = (grabDiff * 2 * Math.PI) / canvas.offsetWidth;
 
-console.log(initRotation)
-	  ballerina.rotation.z = rotateAngle + initRotation
-	  triggerRotation = false;
+    ballerina.rotation.z = rotateAngle + startGrabRotation;
+    triggerRotation = false;
   }
 
   renderer.render(scene, camera);
 }
 
 let isGrabbing = false;
-canvas.style.cursor = 'grab'
-let startOfGrabX 
-let currentGrabX
-let triggerRotation = false
+canvas.style.cursor = "grab";
+let startOfGrabX;
+let currentGrabX;
+let triggerRotation = false;
 
-canvas.addEventListener('mousedown', (e) => {
-    if (!isGrabbing) {
-      startOfGrabX = e.clientX;
-    }
-    canvas.style.cursor = 'grabbing'
-    isGrabbing = true;
+canvas.addEventListener("mousedown", (e) => {
+  if (!isGrabbing) {
+    startOfGrabX = e.clientX;
+  }
+  canvas.style.cursor = "grabbing";
+  isGrabbing = true;
 });
-canvas.addEventListener('mousemove', (e) => {
-    currentGrabX = e.clientX;
-})
-
-canvas.addEventListener('mouseleave', () => {
-	canvas.style.cursor = 'grab'
-	isGrabbing = false;
-	startOfGrabX = undefined
-	if (ballerina) {
-		initRotation = ballerina.rotation.z % ( 2 * Math.PI);
-	}
+canvas.addEventListener("mousemove", (e) => {
+  currentGrabX = e.clientX;
 });
 
-canvas.addEventListener('mouseup', () => {
-	canvas.style.cursor = 'grab'
-	isGrabbing = false;
-	startOfGrabX = undefined
-	if (ballerina) {
-		initRotation = ballerina.rotation.z % ( 2 * Math.PI);
-	}
-});
+function onRelease() {
+  canvas.style.cursor = "grab";
+  isGrabbing = false;
+  startOfGrabX = undefined;
+  if (ballerina) {
+    startGrabRotation = ballerina.rotation.z % (2 * Math.PI);
+    accumRotation += startGrabRotation;
+    console.log("a", (360 * accumRotation) / (2 * Math.PI));
+  }
+}
+
+canvas.addEventListener("mouseleave", onRelease);
+
+canvas.addEventListener("mouseup", onRelease);
