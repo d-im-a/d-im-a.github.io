386cfaac5c5e8305887dacea14303b524a2604fc
Dima <dima@dima.local>
2024-11-27 20:44:43 +0100

    add rotate on drag

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color().setHex(0x303030);
 
 const camera = new THREE.PerspectiveCamera(50, 2, 0.1, 1000);
-camera.position.set(0, 8, 14);
+camera.position.set(0, 6, 14);
 camera.lookAt(0, 5, 0);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
@@ -16,13 +16,21 @@ renderer.setAnimationLoop(animate);
 var floorMaterial = new THREE.MeshBasicMaterial();
 var textureLoader = new THREE.TextureLoader();
 textureLoader.load("./assets/texture.jpg", function (texture) {
-  texture.wrapS = THREE.RepeatWrapping;
-  texture.wrapT = THREE.RepeatWrapping;
+  const floorLength = 200;
   // The texture has loaded, so assign it to your material object. In the
   // next render cycle, this material update will be shown on the plane
   // geometry
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.wrapT = THREE.RepeatWrapping;
+  texture.repeat.set(floorLength / 8, floorLength / 8)
   floorMaterial.map = texture;
   floorMaterial.needsUpdate = true;
+
+  const planeGeometry = new THREE.PlaneGeometry(floorLength, floorLength);
+  const floor = new THREE.Mesh(planeGeometry, floorMaterial);
+  floor.rotation.set(-Math.PI / 2, 0, 0);
+
+  scene.add(floor);
 });
 // const planeGeometry = new THREE.PlaneGeometry(200, 200);
 // const planeMaterial = new THREE.MeshPhongMaterial({
@@ -30,14 +38,6 @@ textureLoader.load("./assets/texture.jpg", function (texture) {
 //   shininess: 0.5,
 // });
 // const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-
-const planeGeometry = new THREE.PlaneGeometry(200, 200);
-const plane = new THREE.Mesh(planeGeometry, floorMaterial);
-plane.rotation.set(-Math.PI / 2, 0, 0);
-
-// Make use of the `TextureLoader` object to handle asynchronus loading and
-// assignment of the texture to your material
-scene.add(plane);
 
 var light = new THREE.DirectionalLight(0x00f000);
 light.position.set(-10, 10, 10);
@@ -71,7 +71,6 @@ gltfLoader.load(
       // You can also check for id / name / type here.
       mesh.material = new THREE.MeshStandardMaterial();
       if (mesh instanceof THREE.Mesh) {
-        console.log(mesh);
         mesh.geometry.translate(
           bboxCenter.x - 0.46,
           bboxCenter.y,
@@ -94,9 +93,60 @@ gltfLoader.load(
   },
 );
 
+let initRotation = 0;
 function animate() {
-  if (ballerina) {
-    ballerina.rotation.z += 0.015;
+
+  // if (ballerina) {
+    // ballerina.rotation.z += 0.015;
+  // }
+  if (!ballerina) {
+	  return;
   }
+
+  if (startOfGrabX) {
+	  // console.log(currentGrabX , startOfGrabX)
+	  const grabDiff = currentGrabX - startOfGrabX
+	  const rotateAngle = grabDiff * 2 * Math.PI / canvas.offsetWidth
+
+console.log(initRotation)
+	  ballerina.rotation.z = rotateAngle + initRotation
+	  triggerRotation = false;
+  }
+
   renderer.render(scene, camera);
 }
+
+let isGrabbing = false;
+canvas.style.cursor = 'grab'
+let startOfGrabX 
+let currentGrabX
+let triggerRotation = false
+
+canvas.addEventListener('mousedown', (e) => {
+    if (!isGrabbing) {
+      startOfGrabX = e.clientX;
+    }
+    canvas.style.cursor = 'grabbing'
+    isGrabbing = true;
+});
+canvas.addEventListener('mousemove', (e) => {
+    currentGrabX = e.clientX;
+})
+
+canvas.addEventListener('mouseleave', () => {
+	canvas.style.cursor = 'grab'
+	isGrabbing = false;
+	startOfGrabX = undefined
+	if (ballerina) {
+		initRotation = ballerina.rotation.z % ( 2 * Math.PI);
+	}
+});
+
+canvas.addEventListener('mouseup', () => {
+	canvas.style.cursor = 'grab'
+	isGrabbing = false;
+	startOfGrabX = undefined
+	if (ballerina) {
+		initRotation = ballerina.rotation.z % ( 2 * Math.PI);
+	}
+});
